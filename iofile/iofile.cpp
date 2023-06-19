f3476502649c202bd5d543ef871027ddb835bf3c
ikhdaamel <ikhda.new45@gmail.com>
2023-06-19 10:32:28 +0700

    menutup file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -16,6 +16,12 @@ int main() {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter 'q'
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' kedalam file
+		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
 }
 	
