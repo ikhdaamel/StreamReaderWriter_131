c4ae6f728e94e7e85f7bae8340dc1131e7783b1f
ikhdaamel <ikhda.new45@gmail.com>
2023-06-19 11:26:51 +0700

    case try

---
+++ excepetion standar/excepetion standar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <exception>						//untuk obyek excception yg akan digunakan
+#include <array>							//untuk obyek array yg akan digunakan
+using namespace std;
+
+int main() {
+	cout << "awal program" << endl;			//penanda 1...
+	try {
+		array <int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen
+		//cout<<data.at(5)<<endl;
+		//memanggil array elemen ke 5
+	}
+}
