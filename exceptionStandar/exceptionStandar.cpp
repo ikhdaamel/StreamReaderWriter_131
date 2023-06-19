4364d6c1b7df43043a52cb4a82b41a293f47ee28
ikhdaamel <ikhda.new45@gmail.com>
2023-06-19 11:15:02 +0700

    membuat catch int a

--- exceptionStandar/exceptionStandar.cpp
+++ exceptionStandar/exceptionStandar.cpp
@@ -7,4 +7,8 @@ int main() {
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "pengecualian akan dieksekusi" << endl;
+	}
 }
