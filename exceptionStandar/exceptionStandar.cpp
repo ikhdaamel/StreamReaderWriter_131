340865d402eaa9694394968a9fc1e8e4dd48b1de
ikhdaamel <ikhda.new45@gmail.com>
2023-06-19 11:17:42 +0700

    perubahan

--- exceptionStandar/exceptionStandar.cpp
+++ exceptionStandar/exceptionStandar.cpp
@@ -11,8 +11,10 @@ int main() {
 		//blok ini akan dieksekusi
 		cout << "pengecualian akan dieksekusi" << endl;
 	}
-	catch (....) {
+	catch (...) {
 		/*jika selain integer maka block ini akan dieksekusi*/
 		cout << "default pengecualian dieksekusi" << endl;
 	}
+
+	return 0;
 }
