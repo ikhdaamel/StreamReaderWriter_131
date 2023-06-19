e75d489b5b895bf77742453a5b82acae928e95c9
ikhdaamel <ikhda.new45@gmail.com>
2023-06-19 11:13:40 +0700

    membuat case try

---
+++ exceptionStandar/exceptionStandar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		throw 0.5; //melemparkan sebuah integer maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+}
